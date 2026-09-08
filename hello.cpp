0fbb9a86b887e01c9f0407b1e2f9ce428396b0e0
Kunal Mittal <kunal.mittal@wissendigital.com>
2026-09-08 15:38:38 +0530

    Km

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+git status	Crucial. Shows which files are modified, staged, or untracked.
+git add <file>	Adds a specific file to the staging area.
+git add .	Adds all changed files in the current directory to staging.
+git add -p	"Patch" mode. Allows you to stage parts of a file interactively.
+git rm <file>	Deletes a file from both the working directory and the index.
+git rm --cached <file>	Removes a file from Git tracking but keeps it on your local disk.
